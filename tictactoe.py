07c236cd4295f17fb742680a9d30b96517a2efbe
Bryan Hirsch <bryhirschPKG@gmail.com>
2020-06-25 05:56:04 -0700

    Used REGEX to check move inputs

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python3
 #A tic tac toe game
 #Plans: make a two human player version (input error checking, win conditions), then add option to play against computer, then look at graphics libs to make GUI
-#todo: check for win conditions/early draw, a better method for switching players each turn, regex for move input checking
-
+#todo: check for win conditions/early draw, a better method for switching players each turn
+import re
+validMoveRegex = re.compile(r'(TOP|MID|BOT)-(L|M|R)')
 
 #    theBoard = {'TOP-L': ' ', 'TOP-M': ' ', 'TOP-R': ' ',
 #                'MID-L': ' ', 'MID-M': ' ', 'MID-R': ' ',
@@ -22,12 +23,22 @@ def chooseSymbol(): #returns symbol chosen for player1, player2 handled by calli
         if player1 == 'X' or player1 == 'O':
             return player1
 
-def isValidMove(board, move): #Returns bool if the move is valid, there space is open
-    if board[move] == ' ':
-        return True
-    else:
-        return False
+def moveInput(player1Turn=True): #default will assume player 1, to call for vs computer match
+    while True:
+        print("Player %d, make your move: ROW-COLUMN=(TOP, MID, BOT)-(L,M,R)" % (1 if player1Turn else 2))
+        try:
+            move = str(input()).upper()
+            if validMoveRegex.search(move) is None:
+                print("Invalid input. Try again.")
+                continue
+            return move
+        except ValueError:
+            print("Invalid input. Try again.")
+            continue
 
+
+def findWinner(): #Not implemented, going to do bulky if blocks, then try to optimize
+    return None
 
 def singleplayer():
     print("Single-player: WIP")
@@ -39,26 +50,27 @@ def multiplayer():
     print("OK, Player 1 is '%s's and Player 2 is '%s's." % (player1, player2))
 
     turn = 0
+    player1Turn = True
     theBoard = {'TOP-L': ' ', 'TOP-M': ' ', 'TOP-R': ' ',
                 'MID-L': ' ', 'MID-M': ' ', 'MID-R': ' ',
                 'BOT-L': ' ', 'BOT-M': ' ', 'BOT-R': ' '}
     printBoard(theBoard) #displaying the empty board
 
-    while turn < 9: #forces all moves to be made, make a function that checks for an early draw
-        print("Player %d, make your move: ROW-COLUMN=(TOP, MID, BOT)-(L,M,R)" % (turn%2+1))
-        move = str(input()).upper() #need to implement input checking function, look up regex for it
-        if isValidMove(theBoard, move):
-            theBoard[move] = player2 if turn%2 else player1
+    while turn < 9: #Only 9 moves max
+        move = moveInput(player1Turn)
+        if theBoard[move] == ' ':
+            theBoard[move] = player1 if player1Turn else player2
             turn += 1   #Move made and turn completed
         else:
-            print("Invalid move.")
+            print("Invalid move. Try again.")
             continue #Re-prompt for a valid move
 
         printBoard(theBoard) #print out the new board at "end" of each turn
-#       if turn > 5
-#           winner = findWinner(theBoard)   #a win can only occur after turn 5
+
+#        if turn > 5:
+#            winner = findWinner(theBoard)   #a win can only occur after turn 5
 #           if winner is not ' ':
-#
+        player1Turn = not player1Turn
 
 def main():
     print('TIME TO PLAY TIC TAC TOE.')
